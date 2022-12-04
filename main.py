84186d15085d173076788c9b39ccbd08d217b6ec
Terence Thurman <>
2022-12-04 07:57:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+choice1 = input("You are at a crossroad. Where would you like to go: left or right? ")
+if choice1 == "left":#If left turn, continue
+  choice2 = input("You made it to the bridge, but its destroyed. Are you going to wait for the next ship or swim? Wait or Swim? ")
+  if choice2 == "wait":#If wait, continue
+    choice3 = input("You have made it to castle, but there are three doors. Choose the color of the door you will enter. red, yellow, or blue? ")
+    if choice3 == "red":
+        print("You've been burned by fire! GAME OVER.")
+    elif choice3 == "yellow":
+      print("You've been eaten by beasts! GAME OVER.")
+    elif choice3 == "blue":
+      print("Congratulations! YOU WIN!!!!")
+    else:
+      print("GAME OVER")
+  else:
+    print("You were atacked by a shark! GAME OVER.")
+else:
+  print("You fell into a hole! GAME OVER.")
